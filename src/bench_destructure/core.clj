102d86444dbc126ebb952adbe2823d944e3f0ba5
Fogus <mefogus@gmail.com>
2021-02-12 15:30:05 -0500

    macro times and loop

--- src/bench_destructure/core.clj
+++ src/bench_destructure/core.clj
@@ -1,64 +1,48 @@
 (ns bench-destructure.core)
 
-(defmacro destructure-n [n & [wrap massage]]
+(defmacro destructure-n [n iterations & [wrap massage]]
   (let [vs    (range n)
         ks    (map #(->> % (str "a") keyword) vs)
         names (map (comp symbol name) ks)
         kvs   (interleave ks vs)]
-    `(let [{:keys ~(vec names)} ~(if wrap
-                                   (if massage
-                                     (list wrap (list massage (list* 'list kvs)))
-                                     (list wrap (list* 'list kvs)))
-                                   (list* 'list kvs))]
-       ~(list* + names))))
+    `(time
+      (dotimes [x# ~iterations]
+        (let [{:keys ~(vec names)} ~(if wrap
+                                      (if massage
+                                        (list wrap (list massage (list* 'list kvs)))
+                                        (list wrap (list* 'list kvs)))
+                                      (list* 'list kvs))]
+          ~(list* + names))))))
 
 (defn bench-destr [iterations]
   (println "destructure-2")
-  (time
-   (dotimes [_ iterations]
-     (destructure-n 2)))
+  (destructure-n 2 iterations)
 
   (println "destructure-4")
-  (time
-   (dotimes [_ iterations]
-     (destructure-n 4)))
+  (destructure-n 4 iterations)
 
   (println "destructure-8")
-  (time
-   (dotimes [_ iterations]
-     (destructure-n 8))))
+  (destructure-n 8 iterations))
 
 (defn bench-phm [iterations]
   (println "PHM.create 2")
-  (time
-   (dotimes [_ iterations]
-     (destructure-n 2 clojure.lang.PersistentHashMap/create seq)))
+  (destructure-n 2 iterations clojure.lang.PersistentHashMap/create seq)
 
   (println "PHM.create 4")
-  (time
-   (dotimes [_ iterations]
-     (destructure-n 4 clojure.lang.PersistentHashMap/create seq)))
+  (destructure-n 4 iterations clojure.lang.PersistentHashMap/create seq)
   
   (println "PHM.create 8")
-  (time
-   (dotimes [_ iterations]
-     (destructure-n 8 clojure.lang.PersistentHashMap/create seq))))
+  (destructure-n 8 iterations clojure.lang.PersistentHashMap/create seq))
 
 (defn bench-pam [iterations]
   (println "PAM.createAsIfByAssoc 2")
-  (time
-   (dotimes [_ iterations]
-     (destructure-n 2 clojure.lang.PersistentArrayMap/createAsIfByAssoc to-array)))
+  (destructure-n 2 iterations clojure.lang.PersistentArrayMap/createAsIfByAssoc to-array)
 
   (println "PAM.createAsIfByAssoc 4")
-  (time
-   (dotimes [_ iterations]
-     (destructure-n 4 clojure.lang.PersistentArrayMap/createAsIfByAssoc to-array)))
+  (destructure-n 4 iterations clojure.lang.PersistentArrayMap/createAsIfByAssoc to-array)
 
   (println "PAM.createAsIfByAssoc 8")
-  (time
-   (dotimes [_ iterations]
-     (destructure-n 8 clojure.lang.PersistentArrayMap/createAsIfByAssoc to-array))))
+  (destructure-n 8 iterations clojure.lang.PersistentArrayMap/createAsIfByAssoc to-array))
 
 (defn bench
   [{:keys [iterations] :as opts :or {iterations 20000}}]
