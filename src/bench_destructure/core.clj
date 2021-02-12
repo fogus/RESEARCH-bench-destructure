c5eb8b8c278ae9b06271ce8ba68624b0b1bbff39
Fogus <mefogus@gmail.com>
2021-02-12 13:41:43 -0500

    loops of 2 4 8

--- src/bench_destructure/core.clj
+++ src/bench_destructure/core.clj
@@ -8,23 +8,24 @@
     `(let [{:keys ~(vec names)} ~(list* list kvs)]
        ~(list* + names))))
 
-(defn go [{:keys [kvs iterations]}]
-  (let [seed (case kvs 0 () 1 '(:a) (range (* 2 kvs)))
-        last-elem  (to-array [nil])]
-    (println "using seed" seed)
-    (time
-     (dotimes [_ iterations]
-       (try
-         (let [{:as m} seed]
-           (aset last-elem 0 m))
-         (catch Exception _
-           (aset last-elem 0 nil)))))
-    (println "iterations: " iterations " of " (aget last-elem 0))))
-
 (defn bench
-  [{:keys [kvs iterations] :as opts :or {kvs 2, iterations 20000}}]
-  (println "Benchmarking with " opts)
+  [{:keys [iterations] :as opts :or {iterations 20000}}]
+  (println "Benchmarking with " iterations "iterations.")
   (println "  Clojure version " *clojure-version*)
-  (go (assoc opts :kvs kvs :iterations iterations)))
+
+  (println "destructure-2")
+  (time
+   (dotimes [_ iterations]
+     (destructure-n 2)))
+  
+  (println "destructure-4")
+  (time
+   (dotimes [_ iterations]
+     (destructure-n 4)))
+
+  (println "destructure-8")
+  (time
+   (dotimes [_ iterations]
+     (destructure-n 8))))
 
 
